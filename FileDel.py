8c1c263127aea76d1360221d7f62d1ca0384cdf6
mohamed abuaita <mabuaita@yahoo.com>
2018-06-20 15:29:03 +0000

    Filedel prod ready

--- FileDel.py
+++ FileDel.py
@@ -8,9 +8,10 @@ import botocore
 import os
 
 DicomFolder = '/DICOM/'
+log = '/home/mddx/logs/filedel.txt'
 region = 'us-west-2'
 now = time.time()
-cutoff = now - (3 * 86400)
+cutoff = now - (2 * 86400)
 #print (strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 print (time.ctime())
 
@@ -40,26 +41,46 @@ def iqcenv():
 def delCandidate(bucket):
 	s3 = boto3.resource("s3")
 	client = boto3.client('s3', 'us-west-2')
-	bucket = s3.Bucket(bucket)
 	buckets = s3.buckets.all()
 	epoch = datetime(1970, 1, 1, tzinfo=timezone.utc)
 
 	arr = []
 	for root, dirs, files in os.walk(DicomFolder):
-		for name in files:
-			filepath = os.path.join(root, name)
+		for fname in files:
+			filepath = os.path.join(root, fname)
 			stat = os.stat(filepath)
 			tstat = stat.st_ctime
 			modified = epoch + timedelta(seconds=tstat)
 			if tstat < cutoff:
-				print (filepath, modified)
+				try:
+					s3.Object(bucket, 'uiqcfolder/uploaded/%s' % fname).load()
+				except botocore.exceptions.ClientError as e:
+					if e.response['Error']['Code'] == "404":
+						# The object does not exist.
+						status = 'not in S3'
+						client.upload_file(filepath, bucket, 'uiqcfolder/uploaded/%s' % fname)
+					else:
+						# Something else has gone wrong.
+						status = 'not in S3'
+						client.upload_file(filepath, bucket, 'uiqcfolder/uploaded/%s' % fname)
+						raise
+				else:
+					# The object does exist.
+					status = 'in S3'
+					os.remove(filepath)
+				with open(log, "a") as logfile:
+					logfile.write(filepath + '\t' + str(modified) + '\t' +  status +'\n')
+	with open(log, "a") as logfile:
+		logfile.write(str(time.ctime()) + '\n')
+		logfile.write("=====================================================================================" + '\n')
+		logfile.close() 
 
 def main():
 #	env = str(os.environ[iqcenv])
 #	print (os.environ[iqcenv])
 	env = iqcenv()
 	if env == None:
-		print ("environment: dev, stage, or prod, not set")
+		print ("iqcenv: test, stage, or prod, not set")
 		sys.exit(1)
 	bucketEnv = ('iqc%s' % env)
 	exists = bucket(bucketEnv)
